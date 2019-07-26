349dd334a5a5dcd81181f6d06d4843383a69aeb2
Xinyu Zhou <zxy@megvii.com>
2019-07-26 13:28:55 +0800

    test combinations of hpargparse.bind

--- tests/test_hputils.py
+++ tests/test_hputils.py
@@ -16,6 +16,9 @@ test_file_dir = Path(BASE_DIR) / "test_files"
 
 @contextlib.contextmanager
 def auto_cleanup_temp_dir():
+    """
+    :return: a `class`:`pathlib.Path` object 
+    """
     try:
         tmpdir = Path(tempfile.mkdtemp(prefix="hpargparse-test"))
         yield tmpdir
@@ -125,3 +128,75 @@ class TestAll(unittest.TestCase):
                 str(test_file_dir / "dict_and_list" / "config.yaml"),
             ]
         )
+
+    def _make_pair(self):
+        hp_mgr = hpman.HyperParameterManager("_")
+        parser = argparse.ArgumentParser()
+        hp_mgr.parse_source('_("a", 1)')
+        return hp_mgr, parser
+
+    def test_bind_actions(self):
+        def do_test(assert_type, regex, **bind_kwargs):
+            hp_mgr, parser = self._make_pair()
+            hpargparse.bind(parser, hp_mgr, **bind_kwargs)
+
+            assertion = {True: self.assertRegex, False: self.assertNotRegex}[
+                assert_type
+            ]
+            assertion(parser.format_help(), regex)
+
+        all_keys = ["save", "load", "list", "detail", "serial-format", "exit"]
+
+        def test_exist(keywords, **bind_kwargs):
+
+            not_exists = all_keys.copy()
+            for k in keywords:
+                assert k in all_keys, (k, all_keys)
+                not_exists.remove(k)
+
+            exist_regex = "|".join("-{}".format(k) for k in keywords)
+            not_exist_regex = "|".join("-{}".format(k) for k in not_exists)
+
+            assert (set(keywords) | set(not_exists)) == set(all_keys)
+
+            if exist_regex:
+                do_test(True, exist_regex, **bind_kwargs)
+            if not_exist_regex:
+                do_test(False, not_exist_regex, **bind_kwargs)
+
+        test_exist(all_keys)
+        test_exist([], inject_actions=False)
+        test_exist(["save", "serial-format", "exit"], inject_actions=["save"])
+        test_exist(["load", "serial-format", "exit"], inject_actions=["load"])
+        test_exist(
+            ["save", "load", "serial-format", "exit"], inject_actions=["save", "load"]
+        )
+        test_exist(["list", "exit"], inject_actions=["list"])
+        test_exist(
+            ["save", "serial-format", "list", "exit"], inject_actions=["save", "list"]
+        )
+        test_exist(
+            ["load", "serial-format", "list", "exit"], inject_actions=["load", "list"]
+        )
+        test_exist(
+            ["save", "load", "serial-format", "list", "exit"],
+            inject_actions=["save", "load", "list"],
+        )
+
+    def test_bind_action_prefix(self):
+        hp_mgr, parser = self._make_pair()
+        hpargparse.bind(parser, hp_mgr, action_prefix="hahaha")
+        self.assertRegex(parser.format_help(), r"--hahaha-")
+
+    def test_bind_serial_format(self):
+        hp_mgr, parser = self._make_pair()
+        hpargparse.bind(parser, hp_mgr, serial_format="pickle")
+
+        with auto_cleanup_temp_dir() as d:
+            path = d / "a.pkl"
+            parser.parse_args(["--hp-save", str(path)])
+
+            with open(path, "rb") as f:
+                x = pickle.load(f)
+
+            self.assertEqual(x["a"], 1)
