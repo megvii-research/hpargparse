08658794a528c61b2c3ba6026325dc66ae65f442
tanxuehan <xuehantan@sohu-inc.com>
2020-05-23 13:47:56 +0800

    minor formatting

--- docs/conf.py
+++ docs/conf.py
@@ -36,7 +36,7 @@ extensions = [
     "sphinx.ext.todo",
     "sphinx.ext.coverage",
     "sphinx.ext.mathjax",
-    "sphinx.ext.viewcode"
+    "sphinx.ext.viewcode",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
